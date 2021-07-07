4cf6e8589a56a554c790dc98c5502b6e6cfcc462
saranjayapal <meetshannu@gmail.com>
2021-07-07 13:59:06 +0100

    added middleware auth to the controllers

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -7,6 +7,11 @@ use App\Models\Companies;
 
 class CompaniesController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
